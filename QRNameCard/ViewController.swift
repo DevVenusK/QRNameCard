b929c68e51a00ac386e1bba517bdf0b70fbaefe1
Aitch <hskim@maxst.com>
2020-08-31 22:58:19 +0900

    Initial Commit

---
+++ QRNameCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRNameCard
+//
+//  Created by 김효성 on 2020/08/31.
+//  Copyright © 2020 김효성. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
